a64a676e88fce23b0eb0264766c7f28f4373f78a
Bobby Bernarddi Widjayanto <bobbybernarddi0@gmail.com>
2026-07-24 13:29:52 +0700

    Praktikum-1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return 'Bobby Bernarddi Widjayanto';
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Hi! Selamat Datang di Website Laravel" . $id;
+});
